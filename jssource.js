f00fe5f80af8019398e816515bbff9f08f23c3b2
Matthew Law <mr.matthewlaw@gmail.com>
2020-12-24 23:18:07 +0000

    change default video

--- jssource.js
+++ jssource.js
@@ -1,6 +1,6 @@
 /* global noUiSlider wNumb */
 var jsonData;
-var defaultVid = "7zlLuLKKQv8";
+var defaultVid = "WDiZay9z5fA";
 var videos = {};
 const sites = new XMLHttpRequest();
 var activeTags = [];
